33a8f0870738e30125cd08ac9dba92b074d6d0aa
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2021-10-14 16:18:35 -0300

    Ejercicio 10 completado

--- ejercicio10.py
+++ ejercicio10.py
@@ -3,12 +3,46 @@
 #Carga la tabla con valores numéricos enteros.
 #Suma todos los elementos de cada fila y todos los elementos de cada columna visualizando los resultados en pantalla.
 
-tabla = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
+import random
 
-suma_filas=0
+matriz = []
 
-for i in range(len(tabla)):
-  for o in range (len(tabla[0])):
-    print(tabla[i][o], end=' ')
-  print()
+cant_filas = 5
+cant_columnas = 5
+
+# Se rellena la matriz con numeros randoms
+while len(matriz) < cant_filas:
+  sub_matriz = []
+  for i in range(cant_columnas):
+    numero_random = random.randint(1,10)
+    sub_matriz.append(numero_random)
+  matriz.append(sub_matriz)
+
+def crear_tabla(matriz):
+  print("TABLA")
+  for i in range(len(matriz)):
+    for o in range (len(matriz[0])):
+      print(matriz[i][o], end=' ')
+    print()
+
+def sumar_filas(matriz):
+  suma_fila = 0
+  for i in range(len(matriz)):
+    for j in range(cant_filas):
+      suma_fila = suma_fila + matriz[i][j]
+    matriz[i].append(suma_fila)
+    suma_fila = 0
+
+def sumar_columnas(matriz):
+  nueva_fila=[]
+  for i in range(cant_columnas):
+    suma = sum([columna[i] for columna in matriz])
+    nueva_fila.append(suma)
+  matriz.append(nueva_fila)
+  # print(nueva_fila)
+  # print(matriz)
+
+sumar_columnas(matriz)
+sumar_filas(matriz)
+crear_tabla(matriz)
 
